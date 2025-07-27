c24496baf1a5b0d35493eb28bc3ce5dabd88061f
lukem <luke.maki@outlook.com>
2025-07-27 18:48:52 -0500

    1d: move the left and right buttons into a single object

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,33 @@
 import "react"
-import {useState} from "react"
-
+import {useState} from "react";
 const App = () => {
-    const [left, setLeft] = useState(0);
-    const [right, setRight] = useState(0);
+    const[clicks, setClicks] = useState({
+        left: 0, right: 0
+    })
+
+    const handleLeftClick = () => {
+        const newClicks = {
+            left: clicks.left + 1,
+            right: clicks.right
+        }
+        setClicks(newClicks)
+    }
+
+    const handleRightClick = () => {
+        const newClicks = {
+            left: clicks.left,
+            right: clicks.right + 1
+        }
+        setClicks(newClicks)
+    }
 
     return(
     <div>
-        <button onClick={() => setLeft(left+1)}>
-            {left}
+        <button onClick={(handleLeftClick)}>
+            left {clicks.left}
         </button>
-        <button onClick={() => setRight(right+1)}>
-            {right}
+        <button onClick={(handleRightClick)}>
+            right {clicks.right}
         </button>
     </div>
     )
