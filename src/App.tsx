735b799252141dfa6f3aa07e05b4751c1f93c95c
lukem <luke.maki@outlook.com>
2025-07-28 18:33:52 -0500

    conditional rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,47 @@
-import "react"
 import {useState} from "react";
-const App = () => {
-    const[clicks, setClicks] = useState({
-        left: 0, right: 0
-    })
 
-    const handleLeftClick = () => {
-        setClicks({...clicks, left: clicks.left + 1})
+const History = (props) => {
+    if (props.allClicks.length === 0) {
+        return (
+            <div>
+                the app is used by pressing the buttons
+            </div>
+        )
     }
-
-    const handleRightClick = () => {
-    setClicks({...clicks, right: clicks.right + 1})
-    }
-
-    return(
-    <div>
-        <button onClick={(handleLeftClick)}>
-            left {clicks.left}
-        </button>
-        <button onClick={(handleRightClick)}>
-            right {clicks.right}
-        </button>
-    </div>
+    else return (
+        <div>
+            button press history: {props.allClicks.join(" ")}
+        </div>
     )
 }
 
+
+const App = () => {
+    const [left, setLeft] = useState(0)
+    const [right, setRight] = useState(0)
+    const [allClicks, setAll] = useState([])
+
+
+    const handleLeftClick = () => {
+        setAll(allClicks.concat('L'))
+        setLeft(left + 1)
+    }
+
+
+    const handleRightClick = () => {
+        setAll(allClicks.concat('R'))
+        setRight(right + 1)
+    }
+
+    return (
+        <div>
+            {left}
+            <button onClick={handleLeftClick}>left</button>
+            <button onClick={handleRightClick}>right</button>
+            {right}
+            <History allClicks={allClicks} />
+        </div>
+    )
+}
 
 export default App
