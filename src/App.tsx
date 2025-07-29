aa60a65a847e867f49aeeafd8687c14d3af80504
lukem <luke.maki@outlook.com>
2025-07-29 17:08:43 -0500

    more event handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,19 @@
 import {useState} from "react";
 
-const History = (props) => {
-    if (props.allClicks.length === 0) {
-        return (
-            <div>
-                the app is used by pressing the buttons
-            </div>
-        )
-    }
-    else return (
-        <div>
-            button press history: {props.allClicks.join(" ")}
-        </div>
-    )
-}
-
 
 const App = () => {
-    const [left, setLeft] = useState(0)
-    const [right, setRight] = useState(0)
-    const [allClicks, setAll] = useState([])
-
-
-    const handleLeftClick = () => {
-        setAll(allClicks.concat('L'))
-        setLeft(left + 1)
-    }
-
-
-    const handleRightClick = () => {
-        setAll(allClicks.concat('R'))
-        setRight(right + 1)
+    const [value, setValue] = useState(10);
+    const setToValue = (newValue) => () =>{
+        console.log("value", newValue);
+        setValue(newValue)
     }
 
     return (
         <div>
-            {left}
-            <button onClick={handleLeftClick}>left</button>
-            <button onClick={handleRightClick}>right</button>
-            {right}
-            <History allClicks={allClicks} />
+            {value}
+            <button onClick={setToValue(1000)}>thousand</button>
+            <button onClick={setToValue(0)}>reset</button>
+            <button onClick={setToValue(value+1)}>increment</button>
         </div>
     )
 }
