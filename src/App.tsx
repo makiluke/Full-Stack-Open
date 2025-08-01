f1453728dad6ae09048155322344a2fa4fb10d9e
lukem <luke.maki@outlook.com>
2025-07-31 19:55:59 -0500

    start on exercise 1.6

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,18 @@
-import {useState, type JSXElementConstructor,
-    type MouseEventHandler,
-    type ReactElement,
-    type ReactNode,
-    type ReactPortal,
-    type SetStateAction
+import {
+    useState
 } from "react";
 
 
-const Button = (props: { onClick: MouseEventHandler<HTMLButtonElement>; text: string}) => (
-    <button onClick={props.onClick}>{props.text}</button>
-)
-
-const Display = (props: {value: number}) => (
-    <div>{props.value}</div>
-)
-
+const Button = (props: {onClick: any:; text: string}) => {
+    const [value, setValue] = useState(0)
+    return <button onClick={() => setValue(value + 1)}>(props.text)</button>
+}
 const App = () => {
-    const [value, setValue] = useState(10);
-    const setToValue = (newValue: SetStateAction<number>)  =>{
-        console.log("value", newValue);
-        setValue(newValue)
-    }
 
     return (
         <div>
-            <Display value={value}/>
-            <Button onClick={() => setToValue(1000)} text = "thousand"/>
-            <Button onClick={() => setToValue(0)} text = "reset"/>
-            <Button onClick={() => setToValue(value+1)} text = "increment"/>
+            <h1>give feedback</h1>
+            <Button text="good" />
         </div>
     )
 }
