981abd7344e2b4c795817cc81ec1f6d2e60725cd
lukem <luke.maki@outlook.com>
2025-08-05 16:55:50 -0500

    part2 initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,15 @@
-import {useState} from 'react'
-import {index, pickRandom, random, range} from "mathjs";
+import type { ReactElement, JSXElementConstructor, ReactNode, ReactPortal } from "react"
 
-
-
-const App = () => {
-    const anecdotes = [
-        'If it hurts, do it more often.',
-        'Adding manpower to a late software project makes it later!',
-        'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-        'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-        'Premature optimization is the root of all evil.',
-        'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-        'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-        'The only way to go fast, is to go well.'
-    ]
-
-    const [selected, setSelected] = useState(0)
+const App = (props: { notes: any }) => {
+    const {notes} = props
 
     return (
         <div>
-            <button onClick={() => {
-                setSelected(pickRandom(range(0,8)));
-            }}>Get an anecdote!</button>
-            <div>{anecdotes[selected]}</div>
+            <h1>Notes</h1>
+            <ul>
+                {notes.map((note: { content: string; id: number }) =>
+                    <li key={note.id}>{note.content}</li>)}
+            </ul>
         </div>
     )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,25 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-// @ts-expect-error standard line
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+const notes = [
+    {
+        id: 1,
+        content: 'HTML is easy',
+        important: true
+    },
+    {
+        id: 2,
+        content: 'Browser can execute only JavaScript',
+        important: false
+    },
+    {
+        id: 3,
+        content: 'GET and POST are the most important methods of HTTP protocol',
+        important: true
+    }
+]
 
+// @ts-expect-error standard
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <App notes={notes} />
+)
