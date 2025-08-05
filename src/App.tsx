55aeaeb220fa13295010e1e1fe46a3c74c96e153
lukem <luke.maki@outlook.com>
2025-08-05 17:00:05 -0500

    part2 refactor notes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import type { ReactElement, JSXElementConstructor, ReactNode, ReactPortal } from "react"
 
+
+const Note = (props:{content:string; id:number})=>{
+    const {content, id} = props
+    return <li key={id}>{content}</li>
+}
+
 const App = (props: { notes: any }) => {
     const {notes} = props
 
@@ -8,7 +14,7 @@ const App = (props: { notes: any }) => {
             <h1>Notes</h1>
             <ul>
                 {notes.map((note: { content: string; id: number }) =>
-                    <li key={note.id}>{note.content}</li>)}
+                    <Note id={note.id} content={note.content}/>)}
             </ul>
         </div>
     )
