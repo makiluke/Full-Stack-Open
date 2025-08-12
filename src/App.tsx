15fd268d547c60e84ed0ddc4422db362c9e17777
lukem <luke.maki@outlook.com>
2025-08-12 17:01:20 -0500

    finish 2b form initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,29 @@
-import Course from './components/Course'
+import { useState } from 'react'
+import Note from './components/Note'
 
-const App = () => {
-    const courses = [
-        {
-        id: 1,
-        name: 'Half Stack application development',
-        parts: [
-            {
-                name: 'Fundamentals of React',
-                exercises: 10,
-                id: 1
-            },
-            {
-                name: 'Using props to pass data',
-                exercises: 7,
-                id: 2
-            },
-            {
-                name: 'State of a component',
-                exercises: 14,
-                id: 3
-            }
-        ]
-    },
-        {
-            name: 'Node.js',
-            id: 2,
-            parts: [
-                {
-                name: 'Routing',
-                exercises: 3,
-                id: 1
-        },
-        {
-            name: 'Middlewares',
-                exercises: 7,
-            id: 2
-        }
-        ]
-        }
-]
-    return <div>
-            {courses.map(course => <Course course={course}/>)}
+
+const App = (props) => {
+    const [notes, setNotes] = useState(props.notes)
+
+    const addNote = (event) => {
+        event.preventDefault();
+        console.log('button clicked', event.target)
+    }
+
+    return (
+        <div>
+            <h1>Notes</h1>
+            <ul>
+                {notes.map(note =>
+                    <Note key={note.id} note={note} />
+                )}
+            </ul>
+            <form onSubmit={addNote}>
+                <input />
+                <button type="submit">save</button>
+            </form>
         </div>
+    )
 }
 
 export default App
