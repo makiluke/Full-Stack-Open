ef0432747e4363dc515774e9faee493ecdfa7f6b
lukem <luke.maki@outlook.com>
2025-08-16 10:25:48 -0500

    2b finish 2.6-2.9

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,68 @@
 import { useState } from 'react'
-import Note from './components/Note'
 
+const App = () => {
+    const [persons, setPersons] = useState([
+        { name: 'Arto Hellas',
+        number: '39-44-5323523'}
+    ])
 
-const App = (props) => {
-    const [notes, setNotes] = useState(props.notes)
+    /* new Name controls the form input element
+    /* when we modify the text this changes newName
+    /* when we add Name using button this uses most recent name
+     */
+    const [newName, setNewName] = useState('Name')
+    const [newNumber, setNewNumber] = useState('Number')
+    const [filter, setFilter] = useState('All')
 
-    const addNote = (event) => {
-        event.preventDefault();
-        console.log('button clicked', event.target)
+    const personsToShow = (filter === 'All')
+    ? persons : persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
+
+    const handleFilterChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(event.target.value)
+        setFilter(event.target.value)
+    }
+
+    const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(event.target.value)
+        setNewName(event.target.value)
+    }
+    const handleNumberChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(event.target.value)
+        setNewNumber(event.target.value)
+    }
+    const addName = (event) => {
+        event.preventDefault()
+        const nameObject = {
+            name: newName,
+            number: newNumber,
+            important: Math.random() > 0.5,
+            id: String(persons.length + 1)
+        }
+        if (persons.some(person => person.name === nameObject.name)) {
+            alert(`${nameObject.name} already in persons`)
+        }
+        else {
+            setPersons(persons.concat(nameObject))
+            setNewName('')
+        }
     }
 
     return (
         <div>
-            <h1>Notes</h1>
-            <ul>
-                {notes.map(note =>
-                    <Note key={note.id} note={note} />
-                )}
-            </ul>
-            <form onSubmit={addNote}>
-                <input />
-                <button type="submit">save</button>
+            <h2>Phonebook</h2>
+            <form>
+                <input value={filter} onChange={handleFilterChange}/></form>
+            <button type="submit">add</button>
+            <h2>add a new</h2>
+            <form onSubmit = {addName}>
+                <input value ={newName} onChange={handleNameChange}/>
+                <input value ={newNumber} onChange={handleNumberChange}/>
+                <button type="submit">add</button>
             </form>
+            <h2>Numbers</h2>
+            <ul>
+                {personsToShow.map((person) => (<li>{person.name} {person.number}</li>))}
+            </ul>
         </div>
     )
 }
